00134e8e1af914378f1763cd24c75faf08f28e39
Akihiro Moritomo <moooti190@gmail.com>
2018-07-18 23:25:52 +0900

    盲目のポーション付け足した

--- src/main/java/com/example/examplemod/WatermelonSplitting/BlockGoldenWatermelon.java
+++ src/main/java/com/example/examplemod/WatermelonSplitting/BlockGoldenWatermelon.java
@@ -7,7 +7,10 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
+import net.minecraft.init.MobEffects;
 import net.minecraft.item.Item;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
@@ -39,9 +42,30 @@ public class BlockGoldenWatermelon extends Block {
             return;
         }
 
+        int posX = pos.getX();
+        int posY = pos.getY();
+        int posZ = pos.getZ();
+
+        double distance = mc.thePlayer.getDistance(posX,posY,posZ);
+        if(distance==8){
+            PotionEffect effect = new PotionEffect(MobEffects.BLINDNESS,10,10);
+            playerIn.addPotionEffect(effect);
+        }
+        System.out.println("distance"+posX+" ,  "+posY+"  ,  "+posZ);
+        System.out.println("distance"+distance);
+
         //メインハンドに棒をもってブロックを叩いた時の処理を下に記す。
-        //EnumFacing angle=mc.thePlayer.getHorizontalFacing();//方向を得る
-         Vec3d angle=mc.thePlayer.getForward();
-        System.out.println(angle);
+        Vec3d direction=mc.thePlayer.getForward();//今向いている方角をVec3d型で受け取る
+        String str = direction.toString().replace("(","").replace(")","").replaceAll(" ","");//Vec3d型をString型に変換
+        String[] angle = str.split(",",0);//String型のx,y,zをそれぞれ配列に分ける
+        double x=Double.parseDouble(angle[0]);
+        double y=Double.parseDouble(angle[1]);
+        double z=Double.parseDouble(angle[2]);
+
+        if(!worldIn.isRemote) {
+            System.out.println(x + " , " + y + " , " + z);//配列を表示する。
+            System.out.println("足し算"+(x+y+z));
+            System.out.println("掛け算"+x*y*z);
+        }
     }
 }
